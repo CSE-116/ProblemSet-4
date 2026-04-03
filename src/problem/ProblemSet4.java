1d65065ddf6ecd112727366e85340104016e758c
jessehartloff <hartloff@buffalo.edu>
2026-04-03 12:15:51 -0400

    Update PS4 starter code for Spring 2026

--- src/problem/ProblemSet4.java
+++ src/problem/ProblemSet4.java
@@ -8,8 +8,16 @@ public class ProblemSet4 {
         return 0;
     }
 
+    public static <T> int maxDegree(Graph<T> graph){
+        return 0;
+    }
+
     public static <T> boolean isConnected(Graph<T> graph) {
         return false;
+    }
+
+    public static <T> int numberOfConnectedComponents(Graph<T> graph) {
+        return 0;
     }
 
     public static <T> boolean detectCycle(Graph<T> graph) {
--- src/tests/TestProblemSet4.java
+++ src/tests/TestProblemSet4.java
@@ -1,11 +1,11 @@
 package tests;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 import problem.ProblemSet4;
 import problem.datastructures.Graph;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class TestProblemSet4 {
 
